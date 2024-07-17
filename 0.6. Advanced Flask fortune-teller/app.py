4cf326a86789608b9e2bbfb6c7a164aeb7404381
matarraanan <matarraanan559@gmail.com>
2024-07-17 23:18:58 +0300

    v

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-import random
 
 app = Flask(__name__)
 
@@ -8,18 +7,23 @@ def home():
     if request.method == 'GET':
         return render_template("home.html")
     else:
-        BM = request.form['BD']
-        return redirect(url_for('fort', BM=BM))
+        BD = request.form['BD']
+        return redirect(url_for('fort', BD=BD))
 
-@app.route('/fortune/<BM>')
-def fort(BM):
-   BM_int = int(BM)
-   num = BM_int % 10
-   forts = ["become a cat", "become a dog", "become a horse", "become a duck", "become a buck", 
-                 "become a chef", "die", "never die", "be happy", "be sad"]
-   ran = forts[num]
-    
-   return render_template("fortune.html", rand=ran)
+@app.route('/fortune/<BD>')
+def fort(BD):
+    try:
+        if len(BD) == 0 or len(BD) > 2:
+            return render_template("fortune.html", rand="enter a normal day", title="Bro, enter your day in a correct format")
+        else:
+            BD_int = int(BD)
+            num = BD_int % 10
+            forts = [0,"become a cat", "become a dog", "become a horse", "become a duck", "become a buck", 
+                     "become a chef", "die", "never die", "be happy", "be sad"]
+            ran = forts[num]
+            return render_template("fortune.html", rand=ran, title="Hey, here's your fortune")
+    except ValueError:
+        return render_template("fortune.html", rand="enter a valid number", title="Invalid input")
 
 if __name__ == '__main__':
     app.run(debug=True)
