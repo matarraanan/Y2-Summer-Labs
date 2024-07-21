652142fd2c92be8e975533374e4796c3f382df5e
matarraanan <matarraanan559@gmail.com>
2024-07-21 18:27:24 +0300

    zsdf

---
+++ auth_lab/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import pyrebase
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+config = {
+    "apiKey": "AIzaSyAY6FWrbQKOFDLQahLl3B4tvhXbUEYnEgw",
+    "authDomain": "auth-e78c4.firebaseapp.com",
+    "projectId": "auth-e78c4",
+    "storageBucket": "auth-e78c4.appspot.com",
+    "messagingSenderId": "869098061082",
+    "appId": "1:869098061082:web:4af3ee8511991ea0af00a3",
+    "databaseURL": " "}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            session['user'] = user
+            session['quotes'] = []
+            return redirect(url_for('home'))
+        except:
+            return " error during sign up"
+    return render_template("signup.html")
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = user
+            if 'quotes' not in session:
+            	return redirect(url_for('home'))
+        except:
+            return " error during sign in"
+    return render_template("signin.html")
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+    	return render_template("home.html")
+    else:
+        quote = request.form['quote']
+        session['quotes'].append(quote)
+        return redirect(url_for('thanks'))
+    
+
+@app.route('/thanks')
+def thanks():
+    return render_template("thanks.html")
+
+@app.route('/display')
+def display():
+	quote = session['quotes']
+	return render_template("display.html", quotes=quote)
+
+@app.route('/signout', methods=['POST'])
+def signout():
+    session["user"] = None
+    return redirect(url_for('signin'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
